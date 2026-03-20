9252b27a60661fd951f9e9ee400969c82584f3ba
prem1505 <anurag1.230101034@iiitbh.ac.in>
2026-03-20 15:27:53 +0530

    :Solution

--- solution_1881D.cpp
+++ solution_1881D.cpp
@@ -1,38 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
     int tc;
-    cin>>tc;
+    cin >> tc;
     while(tc--){
         int n;
-        cin>>n;
-        vector<int>a(n);
-        for(int i=0;i<n;i++){
-            cin>>a[i];
+        cin >> n;
+
+        vector<int> a(n);
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
         }
-        for(int i=0;i<=n;i++){
-        for(int j=2;j*j<=a[i];j++){
-            if(a[i]%j==0){
-                mp[j]++;
-                a[i]=a[i]/j;
+
+        map<int,int> mp;
+
+        for(int i = 0; i < n; i++){
+            int x = a[i];
+            for(int j = 2; j * j <= x; j++){
+                while(x % j == 0){
+                    mp[j]++;
+                    x /= j;
+                }
+            }
+            if(x > 1) mp[x]++;
+        }
+
+        bool ok = true;
+
+        for(auto &p : mp){
+            if(p.second % n != 0){
+                ok = false;
+                break;
             }
         }
-    }
-    for(int auto& p :mp){
-        if(p.second()%n==0){
-            c++;
-        }
-        else{
-            c=0;
-            break;
 
-        }
-    }
-    if(c>=1){
-        cout<<"Yes"<<endl;
-    }
-    else{
-        cout<<"No"<<endl;
-    }
+        if(ok) cout << "Yes\n";
+        else cout << "No\n";
     }
 }
